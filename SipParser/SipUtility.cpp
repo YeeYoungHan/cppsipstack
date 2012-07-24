bd1f5404a4abca3b57a3dd65ef6be68bc631632b
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2012-07-24 07:49:45 +0000

    CSipMutex 를 SipStack 프로젝트에서 SipParser 프로젝트로 이동함. SIP 메시지 작성에 필요한 유틸리티 함수를 추가함.

---
+++ SipParser/SipUtility.cpp
@@ -0,0 +1,87 @@
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#include "SipParserDefine.h"
+#include "SipUtility.h"
+#include "SipMutex.h"
+#include <string>
+
+static CSipMutex gclsMutex;
+static int	giTag;
+static int	giBranch;
+static int	giCallId;
+
+void SipMakeTag( char * pszTag, int iTagSize )
+{
+	int		iTag;
+
+	gclsMutex.acquire();
+	if( giTag <= 0 || giTag > 2000000000 )
+	{
+		giTag = 1;
+	}
+	else
+	{
+		++giTag;
+	}
+
+	iTag = giTag;
+	gclsMutex.release();
+
+	snprintf( pszTag, iTagSize, "%d", iTag );
+}
+
+void SipMakeBranch( char * pszBranch, int iBranchSize )
+{
+	int		iBranch;
+
+	gclsMutex.acquire();
+	if( giBranch <= 0 || giBranch > 2000000000 )
+	{
+		giBranch = 1;
+	}
+	else
+	{
+		++giBranch;
+	}
+
+	iBranch = giBranch;
+	gclsMutex.release();
+
+	snprintf( pszBranch, iBranchSize, "%s-WCSS-%d", VIA_PREFIX, iBranch );
+}
+
+void SipMakeCallIdName( char * pszCallId, int iCallIdSize )
+{
+	int		iCallId;
+
+	gclsMutex.acquire();
+	if( giCallId <= 0 || giCallId > 2000000000 )
+	{
+		giCallId = 1;
+	}
+	else
+	{
+		++giCallId;
+	}
+
+	iCallId = giCallId;
+	gclsMutex.release();
+
+	snprintf( pszCallId, iCallIdSize, "WCSS-%d", iCallId );
+}
