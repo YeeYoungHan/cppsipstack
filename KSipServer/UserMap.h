24ec388fc2d8457e3606d8247263c5ceeebbc876
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2012-08-09 00:59:45 +0000

    KSipServer 프로젝트를 시작함. SimpleSipServer 소스 코드를 KSipServer 소스 코드로 이관함.

---
+++ KSipServer/UserMap.h
@@ -0,0 +1,62 @@
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#ifndef _USER_MAP_H_
+#define _USER_MAP_H_
+
+#include "SipMutex.h"
+#include "SipMessage.h"
+#include <map>
+
+/**
+ * @ingroup SimpleSipServer
+ * @brief SIP 클라이언트 정보 저장 클래스
+ */
+class CUserInfo
+{
+public:
+	std::string m_strIp;
+	int					m_iPort;
+
+	time_t			m_iLoginTime;
+	int					m_iLoginTimeout;
+};
+
+typedef std::map< std::string, CUserInfo > USER_MAP;
+
+/**
+ * @ingroup SimpleSipServer
+ * @brief 로그인한 사용자들의 정보를 저장하는 클래스
+ */
+class CUserMap
+{
+public:
+	CUserMap();
+	~CUserMap();
+
+	bool Insert( CSipMessage * pclsMessage );
+	bool Select( const char * pszUserId, CUserInfo & clsInfo );
+
+private:
+	USER_MAP	m_clsMap;
+	CSipMutex m_clsMutex;
+};
+
+extern CUserMap gclsUserMap;
+
+#endif
