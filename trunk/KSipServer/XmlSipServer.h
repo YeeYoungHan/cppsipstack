54f2009a05df8a61f894fb776ad96c3e785ca595
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2012-08-29 14:02:34 +0000

    = KSipServer =  * IP-PBX 정보 저장 XML 파일 예제를 추가함  * IP-PBX 정보 저장 XML 파일을 파싱하는 클래스를 추가함.

---
+++ trunk/KSipServer/XmlSipServer.h
@@ -0,0 +1,41 @@
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#ifndef _XML_SIP_SERVER_H_
+#define _XML_SIP_SERVER_H_
+
+#include <string>
+
+class CXmlSipServer
+{
+public:
+	CXmlSipServer();
+	~CXmlSipServer();
+
+	std::string		m_strIp;
+	int						m_iPort;
+	std::string		m_strDomain;
+
+	std::string		m_strId;
+	std::string		m_strPassWord;
+
+	bool Parse( const char * pszFileName );
+	void Clear();
+};
+
+#endif
