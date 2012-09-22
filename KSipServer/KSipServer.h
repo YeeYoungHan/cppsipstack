3f6c8c836d86bfee268bd7ab1f4a384dda0173d3
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2012-09-22 10:30:48 +0000

    = KSipServer =  * MS 윈도우 서비스로 등록/삭제할 수 있는 기능을 추가함.

--- KSipServer/KSipServer.h
+++ KSipServer/KSipServer.h
@@ -21,6 +21,23 @@
 
 #define USE_MYSQL
 
+// MS 윈도우 서비스 선언
+#define SERVICE_NAME								"KSipServer"		// 서비스 이름
+#define SERVICE_DISPLAY_NAME				"KSipServer"
+#define SERVICE_DESCRIPTION_STRING	"SIP Server - made by Yee Young Han"
+
+#define CONFIG_FILENAME						"SipServer.xml"
+
 extern bool gbStop;
+
+#ifdef WIN32
+// KSipService.cpp
+void KSipServiceStart();
+char * GetConfigFileName();
+
+// InstallService.cpp
+bool InstallService( );
+bool UninstallService( );
+#endif
 
 #endif
