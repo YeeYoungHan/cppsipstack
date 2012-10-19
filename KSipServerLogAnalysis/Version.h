7c15e9d7112b6900d52776c781a3cc09f9578f42
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2012-10-19 05:45:40 +0000

    = KSipServerLogAnalysis =  * 버전 정보를 추가함.

---
+++ KSipServerLogAnalysis/Version.h
@@ -0,0 +1,33 @@
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define PROGRAM_VERSION "0.01"
+
+/* 버전 설명
+
+= 버전 0.01 ( 2012년 10월 19일 ) =
+ * 일별 메소드 통계 저장 기능을 추가함.
+ * 일별 메소드/IP 통계 저장 기능을 추가함.
+ * 일별 메소드/UserAgent 통계 저장 기능을 추가함.
+
+*/
+
+#endif
