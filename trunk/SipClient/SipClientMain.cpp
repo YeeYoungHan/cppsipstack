209d7243fed3972a26a179f84561a950d248aa09
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2012-10-19 09:53:11 +0000

    = SipClient =  * 소스 코드를 리팩토링함.

---
+++ trunk/SipClient/SipClientMain.cpp
@@ -0,0 +1,107 @@
+#include "SipClient.h"
+
+extern std::string	gstrInviteId;
+
+/**
+ * @ingroup SipClient
+ * @brief 
+ * @param argc 
+ * @param argv 
+ * @returns 
+ */
+int main( int argc, char * argv[] )
+{
+	if( argc != 4 )
+	{
+		printf( "[Usage] %s {sip server ip} {user id} {password}\n", argv[0] );
+		return 0;
+	}
+
+	char * pszServerIp = argv[1];
+	char * pszUserId = argv[2];
+	char * pszPassWord = argv[3];
+	
+	CSipUserAgent clsUserAgent;
+	CSipServerInfo clsServerInfo;
+	CSipStackSetup clsSetup;
+	CSipClient clsSipClient;
+
+	clsServerInfo.m_strIp = pszServerIp;
+	clsServerInfo.m_strUserId = pszUserId;
+	clsServerInfo.m_strPassWord = pszPassWord;
+
+	// N개의 IP주소를 사용하는 호스트에서는 SIP 프로토콜로 사용할 IP주소를 직접 입력해 주세요.
+	// Vmware 등을 사용하는 경우 N개의 IP주소가 호스트에 존재합니다.
+	GetLocalIp( clsSetup.m_strLocalIp );
+
+	// 클라이언트가 SIP 통신에 사용할 포트 번호를 넣어 주세요.
+	clsSetup.m_iLocalUdpPort = 10000;
+
+	// UDP 수신 쓰레드의 기본 개수는 1개이다. 이를 수정하려면 CSipStackSetup.m_iUdpThreadCount 를 수정하면 된다.
+
+	clsUserAgent.m_pclsCallBack = &clsSipClient;
+	clsUserAgent.InsertRegisterInfo( clsServerInfo );
+	gclsSipStack.AddNetworkLog( &clsSipClient );
+
+	clsUserAgent.Start( clsSetup );
+
+	char	szCommand[1024];
+	int		iLen;
+
+	memset( szCommand, 0, sizeof(szCommand) );
+	while( fgets( szCommand, sizeof(szCommand), stdin ) )
+	{
+		if( szCommand[0] == 'Q' || szCommand[0] == 'q' ) break;
+
+		iLen = strlen(szCommand);
+		if( iLen >= 2 && szCommand[iLen-2] == '\r' )
+		{
+			szCommand[iLen-2] = '\0';
+		}
+		else if( iLen >= 1 && szCommand[iLen-1] == '\n' )
+		{
+			szCommand[iLen-1] = '\0';
+		}
+
+		if( szCommand[0] == 'C' || szCommand[0] == 'c' )
+		{
+			CSipCallRtp clsRtp;
+			CSipCallRoute	clsRoute;
+
+			// QQQ: RTP 수신 IP/Port/Codec 를 넣어 주세요.
+			clsRtp.m_strIp = clsSetup.m_strLocalIp;
+			clsRtp.m_iPort = 2000;
+			clsRtp.m_iCodec = 0;
+
+			clsRoute.m_strDestIp = pszServerIp;
+			clsRoute.m_iDestPort = 5060;
+
+			clsUserAgent.StartCall( pszUserId, szCommand + 2, &clsRtp, &clsRoute, gstrInviteId );
+		}
+		else if( szCommand[0] == 'e' || szCommand[0] == 's' )
+		{
+			clsUserAgent.StopCall( gstrInviteId.c_str() );
+			gstrInviteId.clear();
+		}
+		else if( szCommand[0] == 'a' )
+		{
+			CSipCallRtp clsRtp;
+
+			// QQQ: RTP 수신 IP/Port/Codec 를 넣어 주세요.
+			clsRtp.m_strIp = clsSetup.m_strLocalIp;
+			clsRtp.m_iPort = 2000;
+			clsRtp.m_iCodec = 0;
+
+			clsUserAgent.AcceptCall( gstrInviteId.c_str(), &clsRtp );
+		}
+	
+		memset( szCommand, 0, sizeof(szCommand) );
+	}
+
+	if( gstrInviteId.empty() == false )
+	{
+		clsUserAgent.StopCall( gstrInviteId.c_str() );
+	}
+
+	return 0;
+}
