102ace07c3fafd61b3a9818a987e0c57251d0440
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2012-11-22 01:15:03 +0000

    time 관련 유틸리트 함수를 SipPlatform 라이브러리의  TimeString.cpp 로 이관함.

--- trunk/SipPlatform/TimeUtility.cpp
+++ trunk/SipPlatform/TimeUtility.cpp
@@ -1,4 +1,23 @@
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
 #include "SipPlatformDefine.h"
+#include "TimeUtility.h"
 
 #ifdef WIN32
 #include <sys/timeb.h>
@@ -15,3 +34,48 @@ int gettimeofday( struct timeval *tv, struct timezone *tz )
 }
 
 #endif
+
+/**
+ * @ingroup SipPlatform
+ * @brief 두 개의 시간의 차이를 리턴한다.
+ * @param psttOld 시작 시간
+ * @param psttNew 종료 시간
+ * @returns 두 개의 시간의 차이를 리턴한다.
+ */
+int DiffTimeval( struct timeval * psttOld, struct timeval * psttNew )
+{
+	int	iRet;
+
+	if( psttNew->tv_usec >= psttOld->tv_usec )
+	{
+		iRet = ( psttNew->tv_usec - psttOld->tv_usec ) / 1000;
+		iRet += ( psttNew->tv_sec - psttOld->tv_sec ) * 1000;
+	}
+	else
+	{
+		iRet = 1000 + ( psttNew->tv_usec - psttOld->tv_usec ) / 1000;
+		iRet += ( psttNew->tv_sec - psttOld->tv_sec - 1 ) * 1000;
+	}
+
+	return iRet;
+}
+
+/**
+ * @ingroup SipPlatform
+ * @brief ms 동안 대기한다.
+ * @param iMiliSecond ms
+ */
+void MiliSleep( int iMiliSecond )
+{
+#ifdef WIN32
+	Sleep( iMiliSecond );
+#else
+	struct timespec req;
+	struct timespec rem;
+
+	req.tv_sec = (int) iMiliSecond / 1000;
+	req.tv_nsec = (int)( iMiliSecond % 1000 ) * 1000;
+
+	nanosleep( &req, &rem );
+#endif
+}
--- trunk/SipPlatform/TimeUtility.h
+++ trunk/SipPlatform/TimeUtility.h
@@ -16,47 +16,12 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
  */
 
-#ifndef _SIP_PLATFORM_DEFINE_H_
-#define _SIP_PLATFORM_DEFINE_H_
+#ifndef _TIME_UTILITY_H_
+#define _TIME_UTILITY_H_
 
-#ifdef WIN32
-#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
 
-#ifdef _DEBUG
-#define _CRTDBG_MAP_ALLOC
-
-#include <stdlib.h>
-#include <crtdbg.h>
-#endif
-
-#define snprintf		_snprintf
-#define strcasecmp	_stricmp
-#define sleep(x)		Sleep(x*1000)
-
-typedef unsigned __int64 uint64_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned char uint8_t;
-
-typedef __int64 int64_t;
-typedef int int32_t;
-typedef __int16 int16_t;
-typedef char int8_t;
-
-#else
-
-#include <stdint.h>
-
-#endif
-
-#ifdef WIN32
-#include <winsock2.h>
-#else
-#include <sys/time.h>
-#endif
-
-#ifdef WIN32
-int gettimeofday( struct timeval *tv, struct timezone *tz );
-#endif
+int DiffTimeval( struct timeval * psttOld, struct timeval * psttNew );
+void MiliSleep( int iMiliSecond );
 
 #endif
