0d0746bc3aa8f922e24899da75f674b5ce6eecd9
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2012-12-01 05:21:34 +0000

    = SipParser =  * Via branch 및 Call-Id 생성을 좀 더 random 하게 생성하도록 수정함.

--- trunk/SipParser/SipParserDefine.h
+++ trunk/SipParser/SipParserDefine.h
@@ -23,7 +23,7 @@
 
 #define SIP_MESSAGE_MAX_LEN				8192
 #define SIP_TAG_MAX_SIZE					21
-#define SIP_BRANCH_MAX_SIZE				51
+#define SIP_BRANCH_MAX_SIZE				61
 #define SIP_CALL_ID_NAME_MAX_SIZE	51
 
 #define VIA_PREFIX						"z9hG4bK"
