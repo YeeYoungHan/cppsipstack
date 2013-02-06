4851ae7735afbf664a6d5da886592df9ab3cc7fe
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-02-06 01:15:58 +0000

    = McuEmulator =  * MCU Emulator 프로젝트를 시작함.

---
+++ trunk/McuEmulator/SipServer.cpp
@@ -0,0 +1,97 @@
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#include "SipServer.h"
+#include "SipUtility.h"
+
+CSipStack	gclsSipStack;
+CSipServer gclsSipServer;
+
+CSipServer::CSipServer()
+{
+}
+
+CSipServer::~CSipServer()
+{
+}
+
+/**
+ * @ingroup McuEmulator
+ * @brief SIP 서버를 시작한다.
+ * @param clsSetup SIP stack 설정 항목을 저장한 객체
+ * @returns 성공하면 true 를 리턴하고 실패하면 false 를 리턴한다.
+ */
+bool CSipServer::Start( CSipStackSetup & clsSetup )
+{
+	gclsSipStack.AddCallBack( this );
+
+	if( gclsSipStack.Start( clsSetup ) == false ) return false;
+
+	return true;
+}
+
+/**
+ * @ingroup McuEmulator
+ * @brief SIP 요청 메시지 수신 이벤트 핸들러
+ * @param iThreadId		쓰레드 아이디
+ * @param pclsMessage SIP 요청 메시지
+ * @returns SIP 요청 메시지를 처리하면 true 를 리턴하고 그렇지 않으면 false 를 리턴한다.
+ */
+bool CSipServer::RecvRequest( int iThreadId, CSipMessage * pclsMessage )
+{
+	if( pclsMessage->IsMethod( "REGISTER" ) )
+	{
+		CSipMessage * pclsResponse = pclsMessage->CreateResponseWithToTag( SIP_NOT_IMPLEMENTED );
+		if( pclsResponse )
+		{
+			return gclsSipStack.SendSipMessage( pclsResponse );
+		}
+	}
+	else if( pclsMessage->IsMethod( "INVITE" ) )
+	{
+
+	}
+	else if( pclsMessage->IsMethod( "UPDATE" ) || pclsMessage->IsMethod( "INFO" ) )
+	{
+		CSipMessage * pclsResponse = pclsMessage->CreateResponseWithToTag( SIP_OK );
+		if( pclsResponse )
+		{
+			return gclsSipStack.SendSipMessage( pclsResponse );
+		}
+	}
+	else if( pclsMessage->IsMethod( "BYE" ) )
+	{
+
+	}
+
+	return false;
+}
+
+/**
+ * @ingroup McuEmulator
+ * @brief SIP 응답 메시지 수신 이벤트 핸들러
+ * @param iThreadId		쓰레드 아이디
+ * @param pclsMessage SIP 응답 메시지
+ * @returns SIP 응답 메시지를 처리하면 true 를 리턴하고 그렇지 않으면 false 를 리턴한다.
+ */
+bool CSipServer::RecvResponse( int iThreadId, CSipMessage * pclsMessage )
+{
+	
+
+	return false;
+}
