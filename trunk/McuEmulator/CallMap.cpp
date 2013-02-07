6ab08c94eb612070f768bebface352523b641acd
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-02-07 06:38:24 +0000

    = McuEmulator =  * 회의방 입장/퇴장 기능 완성 ( not tested )

--- trunk/McuEmulator/CallMap.cpp
+++ trunk/McuEmulator/CallMap.cpp
@@ -1,6 +1,29 @@
-#include "CallMap.h"
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
 
-CCallMap::CCallMap()
+#include "CallMap.h"
+#include "RoomMap.h"
+#include "RtpThread.h"
+#include "SdpMessage.h"
+
+CCallMap gclsCallMap;
+
+CCallMap::CCallMap() : m_iUdpStartPort(10000), m_iUdpStopPort(20000), m_iUdpPort(0)
 {
 }
 
@@ -20,8 +43,38 @@ bool CCallMap::Insert( CSipMessage * pclsMessage )
 	itMap = m_clsMap.find( strKey );
 	if( itMap == m_clsMap.end() )
 	{
-		
-		bRes = true;
+		std::string strRoomId = pclsMessage->m_clsTo.m_clsUri.m_strUser;
+		CCallInfo	clsCallInfo;
+
+		// 회의방이 존재하지 않으면 생성한다.
+		if( gclsRoomMap.Select( strRoomId.c_str() ) == false )
+		{
+			gclsRoomMap.Insert( strRoomId.c_str() );
+		}
+
+		// RTP relay 에 사용할 로컬 RTP 포트를 생성한다.
+		if( CreatePort( clsCallInfo ) == false )
+		{
+			
+		}
+		else if( StartRtpThread( strRoomId.c_str() ) == false )
+		{
+			clsCallInfo.CloseSocket();
+		}
+		else
+		{
+			clsCallInfo.m_strRoomId = strRoomId;
+			SetRemoteRtp( clsCallInfo, pclsMessage );
+
+			m_clsMap.insert( CALL_MAP::value_type( strKey, clsCallInfo ) );
+			gclsRoomMap.Insert( strRoomId.c_str(), strKey.c_str() );
+
+			bRes = true;
+		}
+	}
+	else
+	{
+		SetRemoteRtp( itMap->second, pclsMessage );
 	}
 	m_clsMutex.release();
 
@@ -31,13 +84,20 @@ bool CCallMap::Insert( CSipMessage * pclsMessage )
 bool CCallMap::Select( CSipMessage * pclsMessage, CCallInfo & clsCallInfo )
 {
 	std::string	strKey;
-	CALL_MAP::iterator	itMap;
-	bool	bRes = false;
 
 	GetKey( pclsMessage, strKey );
 
+	return Select( strKey.c_str(), clsCallInfo );
+}
+
+bool CCallMap::Select( const char * pszKey, CCallInfo & clsCallInfo )
+{
+	std::string	strKey;
+	CALL_MAP::iterator	itMap;
+	bool	bRes = false;
+
 	m_clsMutex.acquire();
-	itMap = m_clsMap.find( strKey );
+	itMap = m_clsMap.find( pszKey );
 	if( itMap != m_clsMap.end() )
 	{
 		clsCallInfo = itMap->second;
@@ -60,6 +120,8 @@ bool CCallMap::Delete( CSipMessage * pclsMessage )
 	itMap = m_clsMap.find( strKey );
 	if( itMap != m_clsMap.end() )
 	{
+		gclsRoomMap.Delete( itMap->second.m_strRoomId.c_str(), strKey.c_str() );
+
 		m_clsMap.erase( itMap );
 		bRes = true;
 	}
@@ -71,4 +133,113 @@ bool CCallMap::Delete( CSipMessage * pclsMessage )
 void CCallMap::GetKey( CSipMessage * pclsMessage, std::string & strKey )
 {
 	pclsMessage->m_clsCallId.ToString( strKey );
+}
+
+bool CCallMap::CreatePort( CCallInfo & clsCallInfo )
+{
+	bool bRes = false;
+
+	if( m_iUdpPort == 0 ) m_iUdpPort = m_iUdpStartPort;
+
+	if( CreatePort( clsCallInfo, m_iUdpPort, m_iUdpStopPort ) )
+	{
+		bRes = true;
+	}
+	else if( m_iUdpPort > m_iUdpStartPort )
+	{
+		if( CreatePort( clsCallInfo, m_iUdpStartPort, m_iUdpPort ) )
+		{
+			bRes = true;
+		}
+	}
+
+	if( bRes )
+	{
+		m_iUdpPort += 4;
+		if( m_iUdpPort >= m_iUdpStopPort ) m_iUdpPort = m_iUdpStartPort;
+	}
+
+	return bRes;
+}
+
+bool CCallMap::CreatePort( CCallInfo & clsCallInfo, int sStart, int sEnd )
+{
+	for( int sPort = sStart; sPort < sEnd; sPort += 4 )
+	{
+		clsCallInfo.m_hAudioRtp = UdpListen( sPort, NULL );
+		if( clsCallInfo.m_hAudioRtp == INVALID_SOCKET ) continue;
+
+		clsCallInfo.m_hAudioRtcp = UdpListen( sPort + 1, NULL );
+		if( clsCallInfo.m_hAudioRtcp == INVALID_SOCKET )
+		{
+			clsCallInfo.CloseSocket();
+			continue;
+		}
+
+		clsCallInfo.m_hVideoRtp = UdpListen( sPort + 2, NULL );
+		if( clsCallInfo.m_hVideoRtp == INVALID_SOCKET )
+		{
+			clsCallInfo.CloseSocket();
+			continue;
+		}
+
+		clsCallInfo.m_hVideoRtcp =  UdpListen( sPort + 3, NULL );
+		if( clsCallInfo.m_hVideoRtcp == INVALID_SOCKET )
+		{
+			clsCallInfo.CloseSocket();
+			continue;
+		}
+
+		clsCallInfo.m_clsLocalRtp.m_iAudioPort = sPort;
+		clsCallInfo.m_clsLocalRtp.m_iVideoPort = sPort + 2;
+
+		return true;
+	}
+
+	return false;
+}
+
+bool CCallMap::SetRemoteRtp( CCallInfo & clsCallInfo, CSipMessage * pclsMessage )
+{
+	if( pclsMessage->m_clsContentType.IsEqual( "application", "sdp" ) && pclsMessage->m_iContentLength > 0 )
+	{
+		CSdpMessage clsSdpMessage;
+
+		if( clsSdpMessage.Parse( pclsMessage->m_strBody.c_str(), pclsMessage->m_strBody.length() ) > 0 )
+		{
+			SDP_MEDIA_LIST::iterator	itList;
+
+			for( itList = clsSdpMessage.m_clsMediaList.begin(); itList != clsSdpMessage.m_clsMediaList.end(); ++itList )
+			{
+				if( !strcmp( itList->m_strMedia.c_str(), "audio" ) )
+				{
+					clsCallInfo.m_clsRemoteRtp.m_iAudioPort = itList->m_iPort;
+					itList->m_iPort = clsCallInfo.m_clsLocalRtp.m_iAudioPort;
+				}
+				else if( !strcmp( itList->m_strMedia.c_str(), "video" ) )
+				{
+					clsCallInfo.m_clsRemoteRtp.m_iVideoPort = itList->m_iPort;
+					itList->m_iPort = clsCallInfo.m_clsLocalRtp.m_iVideoPort;
+				}
+				else
+				{
+					itList->m_iPort = 0;
+				}
+			}
+
+			char	szSdp[4192];
+			int		iSdpLen;
+
+			memset( szSdp, 0, sizeof(szSdp) );
+
+			iSdpLen = clsSdpMessage.ToString( szSdp, sizeof(szSdp) );
+			if( iSdpLen > 0 )
+			{
+				pclsMessage->m_strBody = szSdp;
+				pclsMessage->m_iContentLength = iSdpLen;
+			}
+		}
+	}
+
+	return true;
 }
--- trunk/McuEmulator/CallMap.h
+++ trunk/McuEmulator/CallMap.h
@@ -1,6 +1,25 @@
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
 #ifndef _CALL_MAP_H_
 #define _CALL_MAP_H_
 
+#include "SipUdp.h"
 #include <map>
 #include <string>
 #include "SipMessage.h"
@@ -19,10 +38,35 @@ public:
 class CCallInfo
 {
 public:
+	CCallInfo() : m_hAudioRtp(INVALID_SOCKET), m_hAudioRtcp(INVALID_SOCKET), m_hVideoRtp(INVALID_SOCKET), m_hVideoRtcp(INVALID_SOCKET)
+	{}
+
+	void CloseSocket( Socket & hSocket )
+	{
+		if( hSocket != INVALID_SOCKET )
+		{
+			closesocket( hSocket );
+			hSocket = INVALID_SOCKET;
+		}
+	}
+
+	void CloseSocket()
+	{
+		CloseSocket( m_hAudioRtp );
+		CloseSocket( m_hAudioRtcp );
+		CloseSocket( m_hVideoRtp );
+		CloseSocket( m_hVideoRtcp );
+	}
+
 	CRtpInfo	m_clsLocalRtp;
 	CRtpInfo	m_clsRemoteRtp;
 
 	std::string	m_strRoomId;
+
+	Socket	m_hAudioRtp;
+	Socket	m_hAudioRtcp;
+	Socket	m_hVideoRtp;
+	Socket	m_hVideoRtcp;
 };
 
 // key ´Â SIP Call-ID ÀÌ´Ù.
@@ -36,13 +80,24 @@ public:
 
 	bool Insert( CSipMessage * pclsMessage );
 	bool Select( CSipMessage * pclsMessage, CCallInfo & clsCallInfo );
+	bool Select( const char * pszKey, CCallInfo & clsCallInfo );
 	bool Delete( CSipMessage * pclsMessage );
 
 private:
 	CSipMutex	m_clsMutex;
 	CALL_MAP	m_clsMap;
 
+	int		m_iUdpStartPort;
+	int		m_iUdpStopPort;
+	int		m_iUdpPort;
+
 	void GetKey( CSipMessage * pclsMessage, std::string & strKey );
+
+	bool CreatePort( CCallInfo & clsCallInfo );
+	bool CreatePort( CCallInfo & clsCallInfo, int sStart, int sEnd );
+	bool SetRemoteRtp( CCallInfo & clsCallInfo, CSipMessage * pclsMessage );
 };
+
+extern CCallMap gclsCallMap;
 
 #endif
