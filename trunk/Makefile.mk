73d7455ec459b04a515e9b71a02f0c1dbacd72cb
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-03-26 07:08:04 +0000

    Makefile.mk 를 추가함 라이브러리 및 프로그램의 Makefile 에서 Makefile.mk 를 include 하도록 수정함

---
+++ trunk/Makefile.mk
@@ -0,0 +1,9 @@
+# Makefile for all of C++ SipStack
+#
+# programmer : yee young han ( websearch@naver.com )
+#            : http://blog.naver.com/websearch
+# start date : 2013/03/26
+
+CC=g++
+CFLAGS=-Wall -O2 -D_REENTRANT -g #-pg
+
