008ef2479ca29f31b21179b3eb3d4bf972ddb43c
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-05-24 02:40:49 +0000

    = AndroidSipStack =  * SipParser 빌드를 추가함

--- trunk/AndroidSipStack/jni/Android.mk
+++ trunk/AndroidSipStack/jni/Android.mk
@@ -7,10 +7,21 @@ LOCAL_SRC_FILES := Directory.cpp Log.cpp ServerUtility.cpp SipMd5.cpp SipMutex.c
 
 include $(BUILD_STATIC_LIBRARY)
 
+LOCAL_PATH := ../../SipParser
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := libsipparser
+LOCAL_SRC_FILES := SipAcceptData.cpp SipCallId.cpp SipChallenge.cpp SipContentType.cpp SipCredential.cpp SipCSeq.cpp SipFrom.cpp SipHeader.cpp SipMessage.cpp \
+	SipParameter.cpp SipParameterList.cpp SipReason.cpp SipStatusCode.cpp SipUri.cpp SipUtility.cpp SipVia.cpp
+LOCAL_C_INCLUDES := ../../SipPlatform
+
+include $(BUILD_STATIC_LIBRARY)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := CppSipStack
-LOCAL_STATIC_LIBRARIES := libsipplatform
+LOCAL_STATIC_LIBRARIES := libsipplatform libsipparser
 
 include $(BUILD_SHARED_LIBRARY)
 
