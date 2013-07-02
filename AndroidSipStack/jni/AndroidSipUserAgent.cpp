437edba7eae9ed2dd4c77cbec91f9a61f29312d1
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-07-02 14:49:29 +0000

    = AndroidSipStack =  * CSipUserAgent.InsertRegisterInfo 메소드 호출 기능을 완성함

--- AndroidSipStack/jni/AndroidSipUserAgent.cpp
+++ AndroidSipStack/jni/AndroidSipUserAgent.cpp
@@ -19,32 +19,18 @@
 #include "AndroidSipUserAgent.h"
 #include "SipUserAgent.h"
 #include "AndroidLog.h"
+#include "AndroidSipServerInfo.hpp"
 
 CSipUserAgent gclsUserAgent;
 
 JNIEXPORT jboolean JNICALL Java_com_cppsipstack_SipUserAgent_InsertRegisterInfo( JNIEnv * env, jclass, jobject clsSipServerInfo )
 {
-	jclass cls = env->GetObjectClass( clsSipServerInfo );
-	if( cls == NULL )
-	{
-		AndroidErrorLog( "clsSipServerInfo is not found" );
-		return JNI_FALSE;
-	}
-	
-	jfieldID fid = env->GetFieldID( cls, "m_strIp", "Ljava/lang/String;" );
-	if( fid == NULL )
-	{
-		AndroidErrorLog( "fid is not found" );
-		return JNI_FALSE;
-	}
-	
-	jstring strIp = (jstring)env->GetObjectField( clsSipServerInfo, fid );
-	const char * pszIp = env->GetStringUTFChars( strIp, NULL );
-	
-	AndroidDebugLog( "ip = [%s]", pszIp );
-	
-	env->ReleaseStringUTFChars( strIp, pszIp );
-	
+	CSipServerInfo clsInfo;
+
+	if( GetSipServerInfo( env, clsSipServerInfo, clsInfo ) == false ) return JNI_FALSE;
+
+	gclsUserAgent.InsertRegisterInfo( clsInfo );
+
 	return JNI_TRUE;
 }
 
