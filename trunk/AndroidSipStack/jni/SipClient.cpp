994cbb0d5f400e6f381f47cb61601ab653962a9f
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-07-05 09:47:10 +0000

    = AndroidSipStack =  * SipUserAgent Start 기능 추가중

---
+++ trunk/AndroidSipStack/jni/SipClient.cpp
@@ -0,0 +1,176 @@
+/* 
+ * Copyright (C) 2012 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#include "SipClient.h"
+#include <time.h>
+
+std::string	gstrInviteId;
+CSipClient gclsSipClient;
+
+/**
+ * @ingroup SipClient
+ * @brief SIP REGISTER 응답 메시지 수신 이벤트 핸들러
+ * @param pclsInfo	SIP REGISTER 응답 메시지를 전송한 IP-PBX 정보 저장 객체
+ * @param iStatus		SIP REGISTER 응답 코드
+ */
+void CSipClient::EventRegister( CSipServerInfo * pclsInfo, int iStatus )
+{
+	printf( "EventRegister(%s) : %d\n", pclsInfo->m_strUserId.c_str(), iStatus );
+}
+
+/**
+ * @ingroup SipClient
+ * @brief SIP 통화 요청 수신에 대한 인증 확인 이벤트 핸들러
+ * @param pclsMessage	SIP INVITE 요청 메시지
+ * @return 인증에 성공하면 true 를 리턴하고 그렇지 않으면 false 를 리턴한다.
+ */
+bool CSipClient::EventIncomingRequestAuth( CSipMessage * pclsMessage )
+{
+	return true;
+}
+
+/**
+ * @ingroup SipClient
+ * @brief SIP 통화 요청 수신 이벤트 핸들러
+ * @param	pszCallId	SIP Call-ID
+ * @param pszFrom		SIP From 사용자 아이디
+ * @param pszTo			SIP To 사용자 아이디
+ * @param pclsRtp		RTP 정보 저장 객체
+ */
+void CSipClient::EventIncomingCall( const char * pszCallId, const char * pszFrom, const char * pszTo, CSipCallRtp * pclsRtp )
+{
+	printf( "EventIncomingCall(%s,%s)\n", pszCallId, pszFrom );
+
+	gstrInviteId = pszCallId;
+
+	if( pclsRtp )
+	{
+		printf( "=> RTP(%s:%d) codec(%d)\n", pclsRtp->m_strIp.c_str(), pclsRtp->m_iPort, pclsRtp->m_iCodec );
+	}
+}
+
+/**
+ * @ingroup SipClient
+ * @brief SIP Ring / Session Progress 수신 이벤트 핸들러
+ * @param	pszCallId		SIP Call-ID
+ * @param iSipStatus	SIP 응답 코드
+ * @param pclsRtp			RTP 정보 저장 객체
+ */
+void CSipClient::EventCallRing( const char * pszCallId, int iSipStatus, CSipCallRtp * pclsRtp )
+{
+	printf( "EventCallRing(%s,%d)\n", pszCallId, iSipStatus );
+
+	if( pclsRtp )
+	{
+		printf( "=> RTP(%s:%d) codec(%d)\n", pclsRtp->m_strIp.c_str(), pclsRtp->m_iPort, pclsRtp->m_iCodec );
+	}
+}
+
+/**
+ * @ingroup SipClient
+ * @brief SIP 통화 연결 이벤트 핸들러
+ * @param	pszCallId	SIP Call-ID
+ * @param pclsRtp		RTP 정보 저장 객체
+ */
+void CSipClient::EventCallStart( const char * pszCallId, CSipCallRtp * pclsRtp )
+{
+	printf( "EventCallStart(%s)\n", pszCallId );
+
+	if( pclsRtp )
+	{
+		printf( "=> RTP(%s:%d) codec(%d)\n", pclsRtp->m_strIp.c_str(), pclsRtp->m_iPort, pclsRtp->m_iCodec );
+	}
+}
+
+/**
+ * @ingroup SipClient
+ * @brief SIP 통화 종료 이벤트 핸들러
+ * @param	pszCallId		SIP Call-ID
+ * @param iSipStatus	SIP 응답 코드. INVITE 에 대한 오류 응답으로 전화가 종료된 경우, INVITE 의 응답 코드를 저장한다.
+ */
+void CSipClient::EventCallEnd( const char * pszCallId, int iSipStatus )
+{
+	printf( "EventCallEnd(%s,%d)\n", pszCallId, iSipStatus );
+}
+
+/**
+ * @ingroup SipClient
+ * @brief SIP ReINVITE 수신 이벤트 핸들러
+ * @param	pszCallId	SIP Call-ID
+ * @param pclsRtp		RTP 정보 저장 객체
+ */
+void CSipClient::EventReInvite( const char * pszCallId, CSipCallRtp * pclsRtp )
+{
+	printf( "EventReInvite(%s)\n", pszCallId );
+
+	if( pclsRtp )
+	{
+		printf( "=> RTP(%s:%d) codec(%d)\n", pclsRtp->m_strIp.c_str(), pclsRtp->m_iPort, pclsRtp->m_iCodec );
+	}
+}
+
+/**
+ * @ingroup SipClient
+ * @brief Screened / Unscreened Transfer 요청 수신 이벤트 핸들러
+ * @param pszCallId					SIP Call-ID
+ * @param pszReferToCallId	전화가 전달될 SIP Call-ID
+ * @param bScreenedTransfer Screened Transfer 이면 true 가 입력되고 Unscreened Transfer 이면 false 가 입력된다.
+ * @returns 요청을 수락하면 true 를 리턴하고 그렇지 않으면 false 를 리턴한다.
+ */
+bool CSipClient::EventTransfer( const char * pszCallId, const char * pszReferToCallId, bool bScreenedTransfer )
+{
+	printf( "EventTransfer(%s,%s)\n", pszCallId, pszReferToCallId );
+
+	return false;
+}
+
+/**
+ * @ingroup SipClient
+ * @brief Blind Transfer 요청 수신 이벤트 핸들러
+ * @param pszCallId			SIP Call-ID
+ * @param pszReferToId	전화가 전달될 사용자 아이디
+ * @returns 요청을 수락하면 true 를 리턴하고 그렇지 않으면 false 를 리턴한다.
+ */
+bool CSipClient::EventBlindTransfer( const char * pszCallId, const char * pszReferToId )
+{
+	printf( "EventBlindTransfer(%s,%s)\n", pszCallId, pszReferToId );
+
+	return false;
+}
+
+/**
+ * @ingroup SipClient
+ * @brief SIP MESSAGE 수신 이벤트 핸들러
+ * @param pszFrom			SIP 메시지 전송 아이디
+ * @param pszTo				SIP 메시지 수신 아이디
+ * @param pclsMessage SIP 메시지
+ * @returns 요청을 수락하면 true 를 리턴하고 그렇지 않으면 false 를 리턴한다.
+ */
+bool CSipClient::EventMessage( const char * pszFrom, const char * pszTo, CSipMessage * pclsMessage )
+{
+	char	szContentType[255];
+
+	memset( szContentType, 0, sizeof(szContentType) );
+	pclsMessage->m_clsContentType.ToString( szContentType, sizeof(szContentType) );
+
+	printf( "EventMessage(%s,%s)\n", pszFrom, pszTo );
+	printf( "content-type[%s]\n", szContentType );
+	printf( "body[%s]\n", pclsMessage->m_strBody.c_str() );
+
+	return true;
+}
