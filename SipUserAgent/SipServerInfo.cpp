03886ee2a83983463093f04e5ef89ad1e59268c5
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-09-10 12:42:02 +0000

    = SipUserAgent =  * FreeSwitch 에 REGISTER 되지 않는 소스 코드를 주석 처리함

--- SipUserAgent/SipServerInfo.cpp
+++ SipUserAgent/SipServerInfo.cpp
@@ -128,12 +128,14 @@ CSipMessage * CSipServerInfo::CreateRegister( CSipMessage * pclsResponse )
 	{
 		AddAuth( pclsRequest, pclsResponse );
 
+		/* 아래의 소스 코드 때문에 FreeSwitch 에 REGISTER 되지 않아서 주석 처리함
 		std::string	strToTag;
 
 		if( pclsResponse->m_clsTo.SelectParam( "tag", strToTag ) )
 		{
 			pclsRequest->m_clsTo.InsertParam( "tag", strToTag.c_str() );
 		}
+		*/
 	}
 
 	return pclsRequest;
