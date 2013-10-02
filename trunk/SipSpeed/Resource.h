2c51b0d7dc3a3f2ccb041f358feec4407e993dec
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-10-02 04:36:39 +0000

    = SipSpeed =  * CPS 측정을 위한 화면 구성을 개발중

--- trunk/SipSpeed/Resource.h
+++ trunk/SipSpeed/Resource.h
@@ -2,21 +2,30 @@
 // Microsoft Visual C++ generated include file.
 // Used by SipSpeed.rc
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_SIPSPEED_DIALOG				102
-#define IDP_SOCKETS_INIT_FAILED			103
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_SIPSPEED_DIALOG             102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDC_CALLER_ID                   1000
+#define IDC_CALLER_PW                   1001
+#define IDC_SIP_SERVER_IP               1002
+#define IDC_SIP_SERVER_PORT             1003
+#define IDC_CALLEE_ID                   1004
+#define IDC_CALLEE_PW                   1005
+#define IDC_SIP_DOMAIN                  1006
+#define IDC_CALL_TOTAL_COUNT            1007
+#define IDC_EDIT9                       1008
+#define IDC_CALL_CONCURRENT_COUNT       1008
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
