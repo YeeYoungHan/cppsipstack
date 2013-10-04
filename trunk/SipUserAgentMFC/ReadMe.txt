8471c294b22bf0a95f0fd15e4fcd74aaccd25f9e
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-10-04 01:10:14 +0000

    = SipUserAgentMFC =  * MFC 기반 SipUserAgent callback 기능 라이브러리를 시작함
    = SipClientMFC =
     * SipUserAgentMFC 를 사용하도록 수정함

---
+++ trunk/SipUserAgentMFC/ReadMe.txt
@@ -0,0 +1,24 @@
+﻿============
+SipUserAgentMFC
+============
+
+1. 개요
+
+ * C++ SIP stack / SipUserAgent 를 MFC 에서 편리하게 사용하기 위한 MFC 라이브러리 개발
+ 
+ * 목적
+   - C++ SIP User Agent 를 이용하여서 MFC 기반 SIP callback 라이브러리를 개발한다.
+
+ * 주요 기능
+   - 로그인 기능
+   - 통화 시작/취소/종료 기능
+
+2. 개발자 정보
+  
+ * 이영한
+ * 이메일 : websearch@naver.com
+ * 블로그 : http://blog.naver.com/websearch
+
+3. 라이선스
+
+ * 본 프로젝트의 라이선스는 GPLv3 이고 기업용 라이선스는 websearch@naver.com 으로 문의해 주세요.
---
+++ trunk/SipUserAgentMFC/targetver.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// 다음 매크로는 필요한 최소 플랫폼을 정의합니다. 필요한 최소 플랫폼은
+// 응용 프로그램을 실행하는 데 필요한 기능이 포함된 가장 빠른 버전의 Windows, Internet Explorer
+// 등입니다. 이 매크로는 지정된 버전 이상의 플랫폼 버전에서 사용 가능한 모든 기능을 활성화해야
+// 작동합니다.
+
+// 아래 지정된 플랫폼에 우선하는 플랫폼을 대상으로 하는 경우 다음 정의를 수정하십시오.
+// 다른 플랫폼에 사용되는 해당 값의 최신 정보는 MSDN을 참조하십시오.
+#ifndef WINVER                          // 필요한 최소 플랫폼을 Windows Vista로 지정합니다.
+#define WINVER 0x0600           // 다른 버전의 Windows에 맞도록 적합한 값으로 변경해 주십시오.
+#endif
+
+#ifndef _WIN32_WINNT            // 필요한 최소 플랫폼을 Windows Vista로 지정합니다.
+#define _WIN32_WINNT 0x0600     // 다른 버전의 Windows에 맞도록 적합한 값으로 변경해 주십시오.
+#endif
+
+#ifndef _WIN32_WINDOWS          // 필요한 최소 플랫폼을 Windows 98로 지정합니다.
+#define _WIN32_WINDOWS 0x0410 // Windows Me 이상에 맞도록 적합한 값으로 변경해 주십시오.
+#endif
+
+#ifndef _WIN32_IE                       // 필요한 최소 플랫폼을 Internet Explorer 7.0으로 지정합니다.
+#define _WIN32_IE 0x0700        // 다른 버전의 IE에 맞도록 적합한 값으로 변경해 주십시오.
+#endif
