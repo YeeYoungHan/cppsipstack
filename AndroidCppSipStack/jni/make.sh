80591409a051e18006c1ce4904a01ba0460ac9dd
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-10-19 06:08:55 +0000

    = AndroidSipStack =  * NDK 디버그 모드로 설정함

--- AndroidCppSipStack/jni/make.sh
+++ AndroidCppSipStack/jni/make.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/android-ndk-r8b/ndk-build
+/android-ndk-r8b/ndk-build NDK_DEBUG=1
 cp -f ../obj/local/armeabi/*.a ../../AndroidSipStack/jni/lib
--- AndroidSipStack/jni/make.sh
+++ AndroidSipStack/jni/make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/android-ndk-r8b/ndk-build
+/android-ndk-r8b/ndk-build NDK_DEBUG=1
 cp ../libs/armeabi/libCppSipStack.so ../TestAndroidSipStack/libs/armeabi
 
