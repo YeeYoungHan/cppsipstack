3185b9ea9994ee9eef7d6a6411a8d4d031144f57
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-11-18 06:13:50 +0000

    = KSipServer =  * SipUser 테이블에 GroupId 를 추가함

--- trunk/KSipServer/Sql/SipUser.sql
+++ trunk/KSipServer/Sql/SipUser.sql
@@ -3,10 +3,11 @@ CREATE TABLE SipUser(
   PassWord VARCHAR(20),
   DND CHAR(1),
   CallForward VARCHAR(100),
+  GroupId VARCHAR(20),
   PRIMARY KEY (`Id`)
 );
 
-INSERT INTO SipUser( Id, PassWord, DND, CallForward ) VALUES( '1000', '1234', 'N', '' );
-INSERT INTO SipUser( Id, PassWord, DND, CallForward ) VALUES( '1001', '1234', 'N', '' );
-INSERT INTO SipUser( Id, PassWord, DND, CallForward ) VALUES( '1002', '1234', 'Y', '' );
-INSERT INTO SipUser( Id, PassWord, DND, CallForward ) VALUES( '1003', '1234', 'N', '1001' );
+INSERT INTO SipUser( Id, PassWord, DND, CallForward, GroupId ) VALUES( '1000', '1234', 'N', '', '' );
+INSERT INTO SipUser( Id, PassWord, DND, CallForward, GroupId ) VALUES( '1001', '1234', 'N', '', '' );
+INSERT INTO SipUser( Id, PassWord, DND, CallForward, GroupId ) VALUES( '1002', '1234', 'Y', '', '' );
+INSERT INTO SipUser( Id, PassWord, DND, CallForward, GroupId ) VALUES( '1003', '1234', 'N', '1001', '' );
