18b36c2970decf05c30b7bbcd2f81ad9c8966c62
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2013-11-20 13:50:05 +0000

    MCU 라이브러리로 WIN32 release 라이브러리를 복사하는 기능을 추가함

--- copy_library_to_mcu.bat
+++ copy_library_to_mcu.bat
@@ -1,12 +1,27 @@
 copy /Y SipPlatform\*.h ..\..\MCU\trunk\cppsipstack\include
 copy /Y SipParser\*.h ..\..\MCU\trunk\cppsipstack\include
 copy /Y SdpParser\*.h ..\..\MCU\trunk\cppsipstack\include
+copy /Y SipStack\*.h ..\..\MCU\trunk\cppsipstack\include
 copy /Y SipUserAgent\*.h ..\..\MCU\trunk\cppsipstack\include
 copy /Y ServerPlatform\*.h ..\..\MCU\trunk\cppsipstack\include
 copy /Y XmlParser\*.h ..\..\MCU\trunk\cppsipstack\include
-copy /Y Debug\SipPlatform.lib ..\..\MCU\trunk\cppsipstack\lib32
-copy /Y Debug\SipParser.lib ..\..\MCU\trunk\cppsipstack\lib32
-copy /Y Debug\SdpParser.lib ..\..\MCU\trunk\cppsipstack\lib32
-copy /Y Debug\SipUserAgent.lib ..\..\MCU\trunk\cppsipstack\lib32
-copy /Y Debug\ServerPlatform.lib ..\..\MCU\trunk\cppsipstack\lib32
-copy /Y Debug\XmlParser.lib ..\..\MCU\trunk\cppsipstack\lib32
+
+REM copy WIN32 debug
+
+copy /Y Debug\SipPlatform.lib ..\..\MCU\trunk\cppsipstack\lib32\debug
+copy /Y Debug\SipParser.lib ..\..\MCU\trunk\cppsipstack\lib32\debug
+copy /Y Debug\SdpParser.lib ..\..\MCU\trunk\cppsipstack\lib32\debug
+copy /Y Debug\SipStack.lib ..\..\MCU\trunk\cppsipstack\lib32\debug
+copy /Y Debug\SipUserAgent.lib ..\..\MCU\trunk\cppsipstack\lib32\debug
+copy /Y Debug\ServerPlatform.lib ..\..\MCU\trunk\cppsipstack\lib32\debug
+copy /Y Debug\XmlParser.lib ..\..\MCU\trunk\cppsipstack\lib32\debug
+
+REM copy WIN32 release
+
+copy /Y Release\SipPlatform.lib ..\..\MCU\trunk\cppsipstack\lib32\release
+copy /Y Release\SipParser.lib ..\..\MCU\trunk\cppsipstack\lib32\release
+copy /Y Release\SdpParser.lib ..\..\MCU\trunk\cppsipstack\lib32\release
+copy /Y Release\SipStack.lib ..\..\MCU\trunk\cppsipstack\lib32\release
+copy /Y Release\SipUserAgent.lib ..\..\MCU\trunk\cppsipstack\lib32\release
+copy /Y Release\ServerPlatform.lib ..\..\MCU\trunk\cppsipstack\lib32\release
+copy /Y Release\XmlParser.lib ..\..\MCU\trunk\cppsipstack\lib32\release
