6ea1c710f64b71d0398490501d156c40a39a97c4
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2014-01-05 02:28:04 +0000

    = SipPlatform =  * GetCurrentMiliSecond 메소드를 추가함

--- trunk/SipPlatform/TimeUtility.h
+++ trunk/SipPlatform/TimeUtility.h
@@ -19,6 +19,7 @@
 #ifndef _TIME_UTILITY_H_
 #define _TIME_UTILITY_H_
 
+#include "SipPlatformDefine.h"
 #include <time.h>
 
 #ifdef WIN32
@@ -27,5 +28,6 @@ int gettimeofday( struct timeval *tv, struct timezone *tz );
 
 int DiffTimeval( struct timeval * psttOld, struct timeval * psttNew );
 void MiliSleep( int iMiliSecond );
+uint64_t GetCurrentMiliSecond();
 
 #endif
