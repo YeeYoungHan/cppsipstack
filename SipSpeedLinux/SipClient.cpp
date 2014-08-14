7f66f00e37ccd4fdf74f71ba41f3a07dc96df51e
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2014-08-14 09:08:14 +0000

    = SipSpeedLinux =  * 성능 측정 기능을 개발함

--- SipSpeedLinux/SipClient.cpp
+++ SipSpeedLinux/SipClient.cpp
@@ -18,8 +18,10 @@
 
 #include "SipClient.h"
 #include "SipSpeedSetup.h"
+#include "SipSpeedLinux.h"
 
 CSipClient::CSipClient() : m_bCallerLogin(false), m_bCalleeLogin(false)
+	, m_iCallSuccess(0), m_iCallError(0)
 {
 }
 
@@ -86,7 +88,13 @@ void CSipClient::EventRegister( CSipServerInfo * pclsInfo, int iStatus )
 
 void CSipClient::EventIncomingCall( const char * pszCallId, const char * pszFrom, const char * pszTo, CSipCallRtp * pclsRtp )
 {
+	CSipCallRtp clsRtp;
 
+	clsRtp.m_strIp = gclsSipUserAgent.m_clsSipStack.m_clsSetup.m_strLocalIp;
+	clsRtp.m_iPort = 4002;	
+	clsRtp.m_iCodec = 0;
+
+	gclsSipUserAgent.AcceptCall( pszCallId, &clsRtp );
 }
 
 void CSipClient::EventCallRing( const char * pszCallId, int iSipStatus, CSipCallRtp * pclsRtp )
@@ -96,10 +104,21 @@ void CSipClient::EventCallRing( const char * pszCallId, int iSipStatus, CSipCall
 
 void CSipClient::EventCallStart( const char * pszCallId, CSipCallRtp * pclsRtp )
 {
+	bool bModify = false;
 
+	m_clsMutex.acquire();
+	++m_iCallSuccess;
+	m_clsMutex.release();
+
+	gclsSipUserAgent.StopCall( pszCallId );
 }
 
 void CSipClient::EventCallEnd( const char * pszCallId, int iSipStatus )
 {
-
+	if( iSipStatus != SIP_OK )
+	{
+		m_clsMutex.acquire();
+		++m_iCallError;
+		m_clsMutex.release();
+	}
 }
