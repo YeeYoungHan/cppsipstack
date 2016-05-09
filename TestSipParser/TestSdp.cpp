9611b725026673cbf2e1b8aeacf8eaf2fd09117b
websearch <websearch@4e9253d4-56d5-48ae-a6fd-a8703264de2b>
2016-05-09 03:11:08 +0000

    = SdpParser =  * 특정 payload type 을 제외하고 모든 payload type 및 관련 attribute 를 삭제하는 기능을 추가함  * 특정 payload type 을 최상위로 올리는 기능을 추가함

--- TestSipParser/TestSdp.cpp
+++ TestSipParser/TestSdp.cpp
@@ -141,6 +141,99 @@ static bool Test3( const char * pszText )
 	return true;
 }
 
+static bool Test4( const char * pszText, const char * pszResult )
+{
+	CSdpMessage	clsSdp;
+	int		iPos, iTextLen = (int)strlen(pszText);
+	char	szResult[512];
+
+	memset( szResult, 0, sizeof(szResult) );
+
+	iPos = clsSdp.Parse( pszText, iTextLen );
+	if( iPos == -1 )
+	{
+		printf( "sdp(%s) parse error\n", pszText );
+		return false;
+	}
+
+	if( iPos != iTextLen )
+	{
+		printf( "sdp(%s) parse len(%d) != real len(%d)\n", pszText, iPos, iTextLen );
+		return false;
+	}
+
+	SDP_MEDIA_LIST::iterator itML;
+
+	for( itML = clsSdp.m_clsMediaList.begin(); itML != clsSdp.m_clsMediaList.end(); ++itML )
+	{
+		if( !strcmp( itML->m_strMedia.c_str(), "audio" ) )
+		{
+			SDP_FMT_LIST clsFmtList = itML->m_clsFmtList;
+			SDP_FMT_LIST::iterator itFL;
+
+			for( itFL = clsFmtList.begin(); itFL != clsFmtList.end(); ++itFL )
+			{
+				int iCodec = atoi( itFL->c_str() );
+
+				if( iCodec != 8 && iCodec != 101 )
+				{
+					itML->DeleteFmtAttribute( iCodec );
+				}
+			}
+		}
+	}
+
+	clsSdp.ToString( szResult, sizeof(szResult) );
+	if( strcmp( pszResult, szResult ) )
+	{
+		printf( "sdp(%s) != result(%s)\n", pszResult, szResult );
+		return false;
+	}
+
+	return true;
+}
+
+static bool TestTopCodec( const char * pszText, const char * pszResult, int iCodec )
+{
+	CSdpMessage	clsSdp;
+	int		iPos, iTextLen = (int)strlen(pszText);
+	char	szResult[512];
+
+	memset( szResult, 0, sizeof(szResult) );
+
+	iPos = clsSdp.Parse( pszText, iTextLen );
+	if( iPos == -1 )
+	{
+		printf( "sdp(%s) parse error\n", pszText );
+		return false;
+	}
+
+	if( iPos != iTextLen )
+	{
+		printf( "sdp(%s) parse len(%d) != real len(%d)\n", pszText, iPos, iTextLen );
+		return false;
+	}
+
+	SDP_MEDIA_LIST::iterator itML;
+
+	for( itML = clsSdp.m_clsMediaList.begin(); itML != clsSdp.m_clsMediaList.end(); ++itML )
+	{
+		if( !strcmp( itML->m_strMedia.c_str(), "audio" ) )
+		{
+			itML->MakeTopFmtAttribute( iCodec );
+		}
+	}
+
+	clsSdp.ToString( szResult, sizeof(szResult) );
+	if( strcmp( pszResult, szResult ) )
+	{
+		printf( "sdp(%s) != result(%s)\n", pszResult, szResult );
+		return false;
+	}
+
+	return true;
+}
+
 static bool TestAudioMedia( const char * pszText )
 {
 	CSdpMessage	clsSdp;
@@ -272,6 +365,144 @@ bool TestSdp()
 		"m=audio 49170 RTP/AVP 0\r\n"
 		"m=video 51372 RTP/AVP 99\r\n"
 		"a=rtpmap:99 h263-1998/90000\r\n" ) == false ) return false;
+
+	if( Test4( "v=0\r\n"
+					"o=- 3593775777 3593775777 IN IP4 192.168.184.129\r\n"
+					"s=media\r\n"
+					"c=IN IP4 192.168.184.129\r\n"
+					"t=0 0\r\n"
+					"a=X-nat:0\r\n"
+					"m=audio 11263 RTP/AVP 103 102 104 117 3 0 8 9 101\r\n"
+					"a=rtcp:11264 IN IP4 192.168.184.129\r\n"
+					"a=rtpmap:103 speex/16000\r\n"
+					"a=rtpmap:102 speex/8000\r\n"
+					"a=rtpmap:104 speex/32000\r\n"
+					"a=rtpmap:117 iLBC/8000\r\n"
+					"a=fmtp:117 mode=30\r\n"
+					"a=rtpmap:3 GSM/8000\r\n"
+					"a=rtpmap:0 PCMU/8000\r\n"
+					"a=rtpmap:8 PCMA/8000\r\n"
+					"a=rtpmap:9 G722/8000\r\n"
+					"a=sendrecv\r\n"
+					"a=rtpmap:101 telephone-event/8000\r\n"
+					"a=fmtp:101 0-15\r\n"
+				, "v=0\r\n"
+					"o=- 3593775777 3593775777 IN IP4 192.168.184.129\r\n"
+					"s=media\r\n"
+					"c=IN IP4 192.168.184.129\r\n"
+					"t=0 0\r\n"
+					"a=X-nat:0\r\n"
+					"m=audio 11263 RTP/AVP 8 101\r\n"
+					"a=rtcp:11264 IN IP4 192.168.184.129\r\n"
+					"a=rtpmap:8 PCMA/8000\r\n"
+					"a=sendrecv\r\n"
+					"a=rtpmap:101 telephone-event/8000\r\n"
+					"a=fmtp:101 0-15\r\n") == false ) return false;
+
+	if( TestTopCodec( "v=0\r\n"
+					"o=- 3593775777 3593775777 IN IP4 192.168.184.129\r\n"
+					"s=media\r\n"
+					"c=IN IP4 192.168.184.129\r\n"
+					"t=0 0\r\n"
+					"a=X-nat:0\r\n"
+					"m=audio 11263 RTP/AVP 103 102 104 117 3 0 8 9 101\r\n"
+					"a=rtcp:11264 IN IP4 192.168.184.129\r\n"
+					"a=rtpmap:103 speex/16000\r\n"
+					"a=rtpmap:102 speex/8000\r\n"
+					"a=rtpmap:104 speex/32000\r\n"
+					"a=rtpmap:117 iLBC/8000\r\n"
+					"a=fmtp:117 mode=30\r\n"
+					"a=rtpmap:3 GSM/8000\r\n"
+					"a=rtpmap:0 PCMU/8000\r\n"
+					"a=rtpmap:8 PCMA/8000\r\n"
+					"a=rtpmap:9 G722/8000\r\n"
+					"a=sendrecv\r\n"
+					"a=rtpmap:101 telephone-event/8000\r\n"
+					"a=fmtp:101 0-15\r\n"
+				, "v=0\r\n"
+					"o=- 3593775777 3593775777 IN IP4 192.168.184.129\r\n"
+					"s=media\r\n"
+					"c=IN IP4 192.168.184.129\r\n"
+					"t=0 0\r\n"
+					"a=X-nat:0\r\n"
+					"m=audio 11263 RTP/AVP 8 103 102 104 117 3 0 9 101\r\n"
+					"a=rtcp:11264 IN IP4 192.168.184.129\r\n"
+					"a=rtpmap:8 PCMA/8000\r\n"
+					"a=rtpmap:103 speex/16000\r\n"
+					"a=rtpmap:102 speex/8000\r\n"
+					"a=rtpmap:104 speex/32000\r\n"
+					"a=rtpmap:117 iLBC/8000\r\n"
+					"a=fmtp:117 mode=30\r\n"
+					"a=rtpmap:3 GSM/8000\r\n"
+					"a=rtpmap:0 PCMU/8000\r\n"
+					"a=rtpmap:9 G722/8000\r\n"
+					"a=sendrecv\r\n"
+					"a=rtpmap:101 telephone-event/8000\r\n"
+					"a=fmtp:101 0-15\r\n", 8 ) == false ) return false;
+
+	if( TestTopCodec( "v=0\r\n"
+					"o=- 3593775777 3593775777 IN IP4 192.168.184.129\r\n"
+					"s=media\r\n"
+					"c=IN IP4 192.168.184.129\r\n"
+					"t=0 0\r\n"
+					"a=X-nat:0\r\n"
+					"m=audio 11263 RTP/AVP 103 102 104 117 3 0 8 9 101\r\n"
+					"a=rtcp:11264 IN IP4 192.168.184.129\r\n"
+					"a=rtpmap:103 speex/16000\r\n"
+					"a=rtpmap:102 speex/8000\r\n"
+					"a=rtpmap:104 speex/32000\r\n"
+					"a=rtpmap:117 iLBC/8000\r\n"
+					"a=fmtp:117 mode=30\r\n"
+					"a=rtpmap:3 GSM/8000\r\n"
+					"a=rtpmap:0 PCMU/8000\r\n"
+					"a=rtpmap:8 PCMA/8000\r\n"
+					"a=rtpmap:9 G722/8000\r\n"
+					"a=sendrecv\r\n"
+					"a=rtpmap:101 telephone-event/8000\r\n"
+					"a=fmtp:101 0-15\r\n"
+				, "v=0\r\n"
+					"o=- 3593775777 3593775777 IN IP4 192.168.184.129\r\n"
+					"s=media\r\n"
+					"c=IN IP4 192.168.184.129\r\n"
+					"t=0 0\r\n"
+					"a=X-nat:0\r\n"
+					"m=audio 11263 RTP/AVP 117 103 102 104 3 0 8 9 101\r\n"
+					"a=rtcp:11264 IN IP4 192.168.184.129\r\n"
+					"a=rtpmap:117 iLBC/8000\r\n"
+					"a=fmtp:117 mode=30\r\n"
+					"a=rtpmap:103 speex/16000\r\n"
+					"a=rtpmap:102 speex/8000\r\n"
+					"a=rtpmap:104 speex/32000\r\n"
+					"a=rtpmap:3 GSM/8000\r\n"
+					"a=rtpmap:0 PCMU/8000\r\n"
+					"a=rtpmap:8 PCMA/8000\r\n"
+					"a=rtpmap:9 G722/8000\r\n"
+					"a=sendrecv\r\n"
+					"a=rtpmap:101 telephone-event/8000\r\n"
+					"a=fmtp:101 0-15\r\n", 117 ) == false ) return false;
+
+	if( TestTopCodec( "v=0\r\n"
+					"o=- 3593775777 3593775777 IN IP4 192.168.184.129\r\n"
+					"s=media\r\n"
+					"c=IN IP4 192.168.184.129\r\n"
+					"t=0 0\r\n"
+					"a=X-nat:0\r\n"
+					"m=audio 11263 RTP/AVP 8 101\r\n"
+					"a=rtpmap:8 PCMA/8000\r\n"
+					"a=sendrecv\r\n"
+					"a=rtpmap:101 telephone-event/8000\r\n"
+					"a=fmtp:101 0-15\r\n"
+				, "v=0\r\n"
+					"o=- 3593775777 3593775777 IN IP4 192.168.184.129\r\n"
+					"s=media\r\n"
+					"c=IN IP4 192.168.184.129\r\n"
+					"t=0 0\r\n"
+					"a=X-nat:0\r\n"
+					"m=audio 11263 RTP/AVP 8 101\r\n"
+					"a=rtpmap:8 PCMA/8000\r\n"
+					"a=sendrecv\r\n"
+					"a=rtpmap:101 telephone-event/8000\r\n"
+					"a=fmtp:101 0-15\r\n", 8 ) == false ) return false;
 
 	return true;
 }
