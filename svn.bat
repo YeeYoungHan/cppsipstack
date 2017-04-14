d6a591860d27547ca05e7c0866a94323f36f8525
Yee Young Han <websearch@naver.com>
2017-04-14 11:14:19 +0900

    모든 오픈소스 프로젝트 update 기능

---
+++ svn.bat
@@ -0,0 +1,6 @@
+TortoiseProc.exe /command:update /path:"C:\OpenProject\CppHttpStack\" /closeonend:1
+TortoiseProc.exe /command:update /path:"C:\OpenProject\CppSipStack\" /closeonend:1
+TortoiseProc.exe /command:update /path:"C:\OpenProject\CppSnmpStack\" /closeonend:1
+TortoiseProc.exe /command:update /path:"C:\OpenProject\DbMysqlPool\" /closeonend:1
+TortoiseProc.exe /command:update /path:"C:\OpenProject\DbSQLite\" /closeonend:1
+TortoiseProc.exe /command:update /path:"C:\OpenProject\WinBackup\" /closeonend:1
