55bd0ccb1df95937e2f197bbd79eaa017c4ce6b8
Yee Young Han <websearch@naver.com>
2017-12-26 19:03:29 +0900

    XOR MAPPED ADDRESS 테스트를 추가함

--- TestStunParser/TestStunXorMappedAddress.cpp
+++ TestStunParser/TestStunXorMappedAddress.cpp
@@ -19,10 +19,16 @@
 #include "SipPlatformDefine.h"
 #include "TestStunParser.h"
 
-bool TestStunXorMappedAddress( )
+/**
+ * @ingroup TestStunParser
+ * @brief 입력된 STUN 패킷에서 XOR_MAPPED_ADDRESS 애트리뷰트를 찾은 후, XOR_MAPPED_ADDRESS 의 IP/Port 와 입력된 IP/Port 가 일치하는지 검사한다.
+ * @param pszInput		HEX steam 으로 저장된 STUN 패킷
+ * @param pszInputIp	IP 주소
+ * @param sInputPort	Port 번호
+ * @returns XOR_MAPPED_ADDRESS 의 IP/Port 와 입력된 IP/Port 가 일치하면 true 를 리턴하고 그렇지 않으면 false 를 리턴한다.
+ */
+static bool TestStunXorMappedAddress( const char * pszInput, const char * pszInputIp, uint16_t sInputPort )
 {
-	const char * pszInput = "010100342112a4428e24a0e78f0a741a2ae427770020000800010720e1baa46280700004000000030008001411d451d31710a08026478975871baf48e001ef428028000446cfd3d1";
-	
 	CStunMessage clsMessage;
 	std::string strPacket;
 
@@ -45,21 +51,33 @@ bool TestStunXorMappedAddress( )
 
 			itAL->GetIpPort( strIp, sPort );
 
-			if( strcmp( strIp.c_str(), "192.168.0.32" ) )
+			if( strcmp( strIp.c_str(), pszInputIp ) )
 			{
-				printf( "%s ip error\n", __FUNCTION__ );
+				printf( "%s ip(%s) != input ip(%s) => packet(%s)\n", __FUNCTION__, strIp.c_str(), pszInputIp, pszInput );
 				return false;
 			}
 
-			if( sPort != 9778 )
+			if( sPort != sInputPort )
 			{
-				printf( "%s port error\n", __FUNCTION__ );
+				printf( "%s port(%d) != input port(%d) => packet(%s)\n", __FUNCTION__, sPort, sInputPort, pszInput );
 				return false;
 			}
 
 			break;
 		}
 	}
+
+	return true;
+
+}
+
+bool TestStunXorMappedAddress( )
+{
+	if( TestStunXorMappedAddress( "010100342112a4428e24a0e78f0a741a2ae427770020000800010720e1baa46280700004000000030008001411d451d31710a08026478975871baf48e001ef428028000446cfd3d1"
+				, "192.168.0.32", 9778 ) == false ) return false;
+	
+	if( TestStunXorMappedAddress( "010100342112a442cec325aeb2bb95978bb3615600200008000167dee1baa45d807000040000000300080014494fd190005dbecaa1dc9c169e97cca06a3bbc30802800049befef2b"
+				, "192.168.0.31", 18124 ) == false ) return false;
 
 	return true;
 }
