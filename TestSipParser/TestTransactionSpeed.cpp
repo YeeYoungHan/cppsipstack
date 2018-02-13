fb4f9440320588604c4c812af69a0f658acaed7b
Yee Young Han <websearch@naver.com>
2018-02-13 10:03:41 +0900

    vc++ 2017 에서 빌드 warning 을 제거함

--- TestSipParser/TestTransactionSpeed.cpp
+++ TestSipParser/TestTransactionSpeed.cpp
@@ -16,8 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
  */
 
-#define _CRT_SECURE_NO_WARNINGS
-
+#include "SipPlatformDefine.h"
 #include "SipUdp.h"
 #include "SipUtility.h"
 #include "TimeUtility.h"
