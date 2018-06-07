d5be8f37f85624e5168fba820da394ce70ed353d
Yee Young Han <websearch@naver.com>
2018-06-07 10:23:29 +0900

    Expires 수정하는 방법을 기술함

--- SipClient/SipClientMain.cpp
+++ SipClient/SipClientMain.cpp
@@ -85,6 +85,9 @@ int main( int argc, char * argv[] )
 	clsServerInfo.m_iPort = iServerPort;
 	//clsServerInfo.m_iNatTimeout = 10;
 
+	// Expires 헤더에 300 을 입력하고 싶으면 아래와 같이 설정하면 된다.
+	// clsServerInfo.m_iLoginTimeout = 300;
+
 	// N개의 IP주소를 사용하는 호스트에서는 SIP 프로토콜로 사용할 IP주소를 직접 입력해 주세요.
 	// Vmware 등을 사용하는 경우 N개의 IP주소가 호스트에 존재합니다.
 	GetLocalIp( clsSetup.m_strLocalIp );
