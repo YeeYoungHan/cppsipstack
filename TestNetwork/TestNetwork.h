76bfc9f6567b9f0443ccd41139cd55b22e6c84fb
Yee Young Han <websearch@naver.com>
2019-03-27 11:51:01 +0900

    불필요한 헤더를 삭제함

--- TestNetwork/TestNetwork.h
+++ TestNetwork/TestNetwork.h
@@ -6,13 +6,6 @@
 #include "ServerUtility.h"
 #include "Log.h"
 
-#ifndef WIN32
-#include <sys/select.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
-#endif
-
 bool TestTcpSendBlocking( );
 
 #endif
