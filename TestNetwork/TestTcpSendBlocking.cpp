2723e4b370bfeb223a904b71ae6319e4b3afd36a
Yee Young Han <websearch@naver.com>
2019-03-27 11:43:27 +0900

    select 를 이용한 TCP send timeout 기능 테스트 추가함

--- TestNetwork/TestTcpSendBlocking.cpp
+++ TestNetwork/TestTcpSendBlocking.cpp
@@ -8,6 +8,8 @@ public:
 	bool	m_bRun;
 };
 
+static bool gbUseSelect = true;
+
 THREAD_API TcpClientThread( LPVOID lpParameter )
 {
 	CTcpClientThreadArg * pclsArg = (CTcpClientThreadArg *)lpParameter;
@@ -22,30 +24,70 @@ THREAD_API TcpClientThread( LPVOID lpParameter )
 		char szBuf[8192];
 		int n = 0, iCount = 0;
 
+		if( gbUseSelect )
+		{
+			struct timeval sttTime;
+			FD_SET	wset;
+			bool		bSendReady = false;
+
+			while( 1 )
+			{
+				bSendReady = false;
+
+				for( int i = 0; i < 10; ++i )
+				{
+					FD_ZERO( &wset );
+					FD_SET( hConn, &wset );
+
+					sttTime.tv_sec = 1;
+					sttTime.tv_usec = 0;
+
+					n = select( 1, NULL, &wset, NULL, &sttTime );
+					if( n > 0 && FD_ISSET( hConn, &wset ) )
+					{
+						bSendReady = true;
+						break;
+					}
+				}
+
+				if( bSendReady == false )
+				{
+					CLog::Print( LOG_DEBUG, "timeout" );
+					break;
+				}
+
+				CLog::Print( LOG_DEBUG, "before send" );
+				n = send( hConn, szBuf, sizeof(szBuf), 0 );
+				CLog::Print( LOG_DEBUG, "after send n(%d) count(%d)", n, iCount );
+			}
+		}
+		else
+		{
 #ifdef WIN32
-		int iTimeout = 10000;
+			int iTimeout = 10000;
 
-		n = setsockopt( hConn, SOL_SOCKET, SO_SNDTIMEO, (const char*)&iTimeout, sizeof(iTimeout) );
+			n = setsockopt( hConn, SOL_SOCKET, SO_SNDTIMEO, (const char*)&iTimeout, sizeof(iTimeout) );
 #else
-		struct timeval sttTime;
+			struct timeval sttTime;
 
-		sttTime.tv_sec = 10;
-		sttTime.tv_usec = 0;
+			sttTime.tv_sec = 10;
+			sttTime.tv_usec = 0;
 
-		n = setsockopt( hConn, SOL_SOCKET, SO_SNDTIMEO, (const char*)&sttTime, sizeof(sttTime) );
+			n = setsockopt( hConn, SOL_SOCKET, SO_SNDTIMEO, (const char*)&sttTime, sizeof(sttTime) );
 #endif
 
-		CLog::Print( LOG_DEBUG, "setsockopt n(%d)", n );
+			CLog::Print( LOG_DEBUG, "setsockopt n(%d)", n );
 
-		while( 1 )
-		{
-			++iCount;
+			while( 1 )
+			{
+				++iCount;
 
-			CLog::Print( LOG_DEBUG, "before send" );
-			n = send( hConn, szBuf, sizeof(szBuf), 0 );
-			CLog::Print( LOG_DEBUG, "after send n(%d) count(%d)", n, iCount );
+				CLog::Print( LOG_DEBUG, "before send" );
+				n = send( hConn, szBuf, sizeof(szBuf), 0 );
+				CLog::Print( LOG_DEBUG, "after send n(%d) count(%d)", n, iCount );
 
-			if( n <= 0 ) break;
+				if( n <= 0 ) break;
+			}
 		}
 
 		closesocket( hConn );
