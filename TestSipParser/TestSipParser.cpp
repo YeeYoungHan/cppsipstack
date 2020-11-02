7cda642b7b9d7e285dfb78266ca651e95a7a5a3d
Yee Young Han <websearch@naver.com>
2020-11-02 11:42:47 +0900

    성능 튜닝 프로젝트를 모두 제거함

--- TestSipParser/TestSipParser.cpp
+++ TestSipParser/TestSipParser.cpp
@@ -53,7 +53,7 @@ bool TestFile( const char * pszFileName );
  */
 int main( int argc, char * argv[] )
 {
-#ifdef WIN32
+#ifdef _DEBUG
 	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF | _CRTDBG_CHECK_ALWAYS_DF );
 #endif
 
