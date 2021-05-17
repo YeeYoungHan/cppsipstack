9e0d2e9f18a39baf1cd4520b30e8ff0b8a634995
Yee Young Han <websearch@naver.com>
2021-05-17 15:54:39 +0900

    32bit, 64bit 구분하여서 복사하도록 수정함

--- AndroidCppSipStack/jni/make.sh
+++ AndroidCppSipStack/jni/make.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 /android-ndk-r12b/build/ndk-build NDK_DEBUG=1
-cp -f ../obj/local/armeabi/*.a ../../AndroidSipStack/jni/lib
+cp -f ../obj/local/armeabi-v7a/*.a ../../AndroidSipStack/jni/lib/armeabi-v7a
+cp -f ../obj/local/arm64-v8a/*.a ../../AndroidSipStack/jni/lib/arm64-v8a
